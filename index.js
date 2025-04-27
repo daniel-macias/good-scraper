4d6fb0d7a55f44d43b474935eae494fa235092d9
daniel-macias <josedanielmr@gmail.com>
2025-04-26 20:05:27 -0600

    testing with args

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const { chromium } = require('playwright');
 
 async function main() {
+	const url = process.argv[2]; // get the URL passed as an argument
+
+	if (!url) {
+		console.error('Error: No URL provided.\nUsage: node index.js <url>');
+		process.exit(1);
+	}
+
 	const browser = await chromium.launch({ headless: true });
 	const page = await browser.newPage();
-	await page.goto('https://www.wikipedia.org/');
+	await page.goto(url);
 
 	const pageTitle = await page.title();
 	console.log('Page title:', pageTitle);
